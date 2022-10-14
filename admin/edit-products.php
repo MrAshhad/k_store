e375889425d6e7cc604e31a9749e9418764c245d
ashhad <zack.axh.001@gmail.com>
2022-10-14 12:13:27 -0700

    mmcommit

--- admin/edit-products.php
+++ admin/edit-products.php
@@ -7,10 +7,12 @@
     $p_category = $_POST["category"];
     $o_img = $_POST["old-image"];
     $date = date("D - M - Y");
+    session_start();
+    $author = $_SESSION["user_id"];
 
 
     include "config.php";
-    $query = "UPDATE `post` SET `title`='{$p_title}',`description`='{$p_desc}',`category`='{$p_category}',`post_date`='{$date}',`author`='{}',`post_img`='{$o_img}' WHERE `post_id` = '{$_pid}';";
+    $query = "UPDATE `post` SET `title`='{$p_title}',`description`='{$p_desc}',`category`='{$p_category}',`post_date`='{$date}',`author`='{$author}',`post_img`='{$o_img}' WHERE `post_id` = '{$_pid}';";
     $result = mysqli_query($conn, $query);
 
     header("location:http://localhost/k_store/admin/products.php")
