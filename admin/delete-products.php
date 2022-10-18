7d81af2be9114ecb4f059ada081e02c612a45241
ashhad <zack.axh.001@gmail.com>
2022-10-18 09:17:36 +0500

    initial commit

--- admin/delete-products.php
+++ admin/delete-products.php
@@ -2,6 +2,7 @@
 $postid = $_GET["id"];
 include "config.php";
 $Query = "DELETE FROM `post` WHERE `post_id` = '{$postid}'";
-mysqli_query($conn,$Query);
+$query .= "UPDATE `category` SET `post` = post - 1 WHERE `category_id` = {$category};";
+mysqli_multi_query($conn,$Query);
 header("location:http://localhost/k_store/admin/products.php");
 ?>
